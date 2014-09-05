22b8a7ab4907a2c66c2214e815f0bcde22780dc1
Ori <opleban@gmail.com>
2014-09-05 16:41:05 -0700

    modifies create_user_table migration * changes water_service_provider_id to provider_id * adds remember_token index * removes extra whitespace

--- db/migrate/20140905194012_create_users.rb
+++ db/migrate/20140905194012_create_users.rb
@@ -2,12 +2,12 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :type
-      t.integer :water_service_provider_id
+      t.integer :provider_id
       t.string :name
       t.string :email
       t.string :password_hash
+      t.index :remember_token
       t.string :phone_number
-
       t.timestamps
     end
   end
