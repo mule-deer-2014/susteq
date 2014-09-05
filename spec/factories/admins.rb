e14850d283d407fa83f8868b0ca052acde4f612a
Whitney O'Banner <wobanner@gmail.com>
2014-09-04 19:06:27 -0700

    Add data to admin factory
    * Add fake first_name, last_name for admin factory

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,6 +2,8 @@ require 'faker'
 
 FactoryGirl.define do
   factory :admin do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
   end
