51f0cb7804922df95aac378757ae96bb65844dab
Ori <opleban@gmail.com>
2014-09-04 21:01:42 -0700

    Adds green and red marker icons, adds renderMarker function

--- map_test/hubmap-controller.js
+++ map_test/hubmap-controller.js
@@ -1,6 +1,6 @@
 //Initialize Map
 //Take in data
-//initialize models
+//parse JSON data and initialize models
 //Create markers
 //Create popups
 //set event listeners
@@ -18,7 +18,10 @@ HubMap.Controller.prototype = {
       url:"/hubs",
       method:"get"
     }).
-    done(this.parseJsonHubData.bind(this)).
+    done(function(){
+      this.parseJsonHubData.bind(this);
+      this.view.renderMarkers(this.hubs);
+    }).
     fail();
   },
 
@@ -27,11 +30,13 @@ HubMap.Controller.prototype = {
       var hubJsonData = jsonData[i];
       if (hubJsonData.latitude && hubJsonData.longitude){
         hub = new Hub(hubJsonData);
-        console.log(hub);
         this.hubs.push(hub);
       }
     }
-  }
+  },
+
+
+
 };
 
 testData = {id:"2", name:"Roger Moooore", created_at:"10/20/2014", town:"Kibera", postalCode:"93420", province:"Eastern", country:"Kenya", latitude:34.06543, longitude:-4.96194, waterPrice:2.02, status:"ok"}
@@ -42,3 +47,4 @@ var mapView = new HubMap.View(34.06543, -4.96194, 15);
 mapView.setEsriTileLayer();
 var mapController = new HubMap.Controller(mapView);
 mapController.parseJsonHubData([testData, testData1])
+mapController.view.renderMarkers(mapController.hubs)
--- map_test/hubmap-view.js
+++ map_test/hubmap-view.js
@@ -3,12 +3,31 @@ HubMap.View = function(startLat, startLong, startZoom){
 };
 
 HubMap.View.prototype = {
+  greenMarker: L.AwesomeMarkers.icon({
+    markerColor: 'green'
+  }),
+
+  redMarker: L.AwesomeMarkers.icon({
+    icon: 'circle',
+    markerColor: 'red'
+  }),
 
   setEsriTileLayer: function(){
-    L.esri.basemapLayer("Imagery").addTo(this.map);
+    L.esri.basemapLayer("Streets").addTo(this.map);
   },
 
   initializeMap: function(lat,longi, zoom){
     this.map = L.map('map').setView([lat, longi], zoom);
+  },
+
+  renderMarkers:function(hubs){
+    for (var i=0; i<hubs.length; i++){
+      if hubs[i]
+      var marker = L.marker([hubs[i].latitude, hubs[i].longitude], {icon:this.greenMarker})
+      popup = L.popup()
+        .setContent('<p>'+ hubs[i].name +"<br /> Water price: $" + hubs[i].waterPrice + ".</p>")
+      marker.bindPopup(popup).addTo(this.map);
+      marker.on("click", marker.openPopup);
+    }
   }
 };
