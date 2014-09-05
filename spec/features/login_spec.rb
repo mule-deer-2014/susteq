48ebbcbf340b00cab34f0624e7c34dfcba1058b7
Whitney O'Banner <wobanner@gmail.com>
2014-09-04 19:10:00 -0700

    Set expectations in user login test
    After login:
    * Test current path is valid
    * Test page contains admin first name

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -8,5 +8,8 @@ feature 'User login' do
     fill_in 'Email', with: admin.email  
     fill_in 'Password', with: admin.password  
     click_link 'Log In'
+
+    expect(current_path).to eq(admins_path)
+    expect(page).to have_content(admin.first_name)
   end
 end
