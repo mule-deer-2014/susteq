b8d7c2d0860a4ed44dd2819ca6c4fac143730253
Whitney O'Banner <wobanner@gmail.com>
2014-09-04 21:54:18 -0700

    Add feature test for admin
    * Test admin can see all hubs via nav link

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -10,4 +10,12 @@ feature 'Admin' do
     expect(current_path).to eq(wsps_path)
     expect(page).to have_content(providers.last.first_name)
   end
+
+  scenario 'can view all hubs' do
+    hubs = Hub.all
+    click_link 'All Hubs'
+    
+    expect(current_path).to eq(hubs_path)
+    expect(page).to have_content(hubs.last.latitude)
+  end
 end
