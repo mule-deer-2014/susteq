290e2b4e24d2e29334c1f37dc6426f1d9446ed5c
Merissa McRissa <apprentice@DBC-Student-Personal.local>
2014-09-04 21:33:02 -0700

    added password security and user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905023538) do
+ActiveRecord::Schema.define(version: 20140905023539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 20140905023538) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password_hash"
+    t.string   "password_digest"
+    t.string   "password"
+    t.string   "password_confirmation"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,14 @@
 require '../rails_helper'
 
-describe User, :type => :model do
+describe User do
   it "has all attributes" do
-  @user = FactoryGirl.create(:user)
-    expect(@user.first_name.nil?).to eq(false)
-    expect(@user.last_name.nil?).to eq(false)
-    expect(@user.email.nil?).to eq(false)
+    user = FactoryGirl.create(:user)
+    expect(user.first_name.nil?).to eq(false)
+    expect(user.last_name.nil?).to eq(false)
+    expect(user.email.nil?).to eq(false)
+    expect(user.password.nil?).to eq(false)
+    expect(user.password_digest.nil?).to eq(false)
   end
+
 end
 
