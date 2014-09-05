5b34d7a5820f0f57530e15940a6917a34a56f200
Whitney O'Banner <wobanner@gmail.com>
2014-09-04 20:47:22 -0700

    Add feature tests for login scenario
    * Test error message display after invalid login
    * Remove trailing whitespace

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -6,8 +6,8 @@ feature 'User logs in' do
 
   scenario 'provider sees wsp dashboard' do
     visit root_path
-    fill_in 'Email', with: provider.email  
-    fill_in 'Password', with: provider.password  
+    fill_in 'Email', with: provider.email
+    fill_in 'Password', with: provider.password
     click_link 'Log In'
 
     expect(current_path).to eq(wsps_path(provider.id))
@@ -16,11 +16,31 @@ feature 'User logs in' do
 
   scenario 'admin sees admin dashboard' do
     visit admins_path
-    fill_in 'Email', with: admin.email  
-    fill_in 'Password', with: admin.password  
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
     click_link 'Log In'
 
     expect(current_path).to eq(admins_path(admin.id))
     expect(page).to have_content(admin.first_name)
+  end
+
+  scenario 'login error displays on login page with invalid email' do
+    visit root_path
+    fill_in 'Email', with: 'invalidemail'
+    fill_in 'Password', with: provider.password
+    click_link 'Log In'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("ERROR")
+  end
+
+  scenario 'login error displays on login page with invalid password' do
+    visit admins_path
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: '_asdfgfsasdf;'
+    click_link 'Log In'
+
+    expect(current_path).to eq(admins_path)
+    expect(page).to have_content("ERROR")
   end
 end
