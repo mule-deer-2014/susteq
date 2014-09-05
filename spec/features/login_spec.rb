3288952a25f9d69eaeb2abcd8d5cdaba36c073bd
Whitney O'Banner <wobanner@gmail.com>
2014-09-04 20:05:06 -0700

    Test login for water service provider
    * Clarify test feature/scenario descriptions

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,9 +1,20 @@
-feature 'User login' do
+feature 'User logs in' do
   background do
     admin = FactoryGirl.create(:admin)
+    provider = FactoryGirl.create(:wsp)
   end
 
-  scenario 'logs in an existing admin' do
+  scenario 'provider sees wsp dashboard' do
+    visit root_path
+    fill_in 'Email', with: provider.email  
+    fill_in 'Password', with: provider.password  
+    click_link 'Log In'
+
+    expect(current_path).to eq(wsps_path)
+    expect(page).to have_content(provider.first_name)
+  end
+
+  scenario 'admin sees admin dashboard' do
     visit admins_path
     fill_in 'Email', with: admin.email  
     fill_in 'Password', with: admin.password  
