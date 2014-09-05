98c98145143d1b474a45b1385a51103fe147cb75
Ori <opleban@gmail.com>
2014-09-05 16:11:35 -0700

    changes association so that a water_service_provider has_many users, not employees a subclass of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-
+  belongs_to :water_service_provider
 
 end
--- app/models/water_service_provider.rb
+++ app/models/water_service_provider.rb
@@ -1,4 +1,4 @@
 class WaterServiceProvider < ActiveRecord::Base
-  has_many :employees
+  has_many :users
   has_many :hubs
 end
