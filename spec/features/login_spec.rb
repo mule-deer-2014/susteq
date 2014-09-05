ace4dc72f846913ba66eddce13508ad07235d9d6
Whitney O'Banner <wobanner@gmail.com>
2014-09-04 18:04:05 -0700

    Add user login test
    * Test login for existing admin user with factory

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,7 +1,12 @@
-feature 'feature' do
+feature 'User login' do
   background do
+    admin = create(:admin)
   end
 
-  scenario 'scenario' do
+  scenario 'logs in an existing admin' do
+    visit root_path
+    fill_in 'Email', with: admin.email  
+    fill_in 'Password', with: admin.password  
+    click_link 'Log In'
   end
 end
