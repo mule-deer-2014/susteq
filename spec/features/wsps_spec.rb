263482d6cbf130fb4f7d13835dce7d65aad95a3a
Whitney O'Banner <wobanner@gmail.com>
2014-09-05 13:46:14 -0700

    Update test for adding new user as a WSP
    * Test new user form contains email field

--- spec/features/wsps_spec.rb
+++ spec/features/wsps_spec.rb
@@ -6,5 +6,6 @@ feature 'Water Service Provider adding a user' do
   scenario 'can fill out form to create a new user' do
     click_link 'Add User' 
     expect(current_path).to eq(new_wsp_path)
+    page.has_field?('email')
   end
 end
