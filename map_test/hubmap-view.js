00739d9f969b0196b2daeaecb39a0470ebdbd729
Ori <opleban@gmail.com>
2014-09-04 21:17:08 -0700

    pulls out individual marker instantiation from renderMarkers function and adds logic to distinguish between 'ok' and 'not ok' status

--- map_test/hubmap-view.js
+++ map_test/hubmap-view.js
@@ -20,11 +20,16 @@ HubMap.View.prototype = {
     this.map = L.map('map').setView([lat, longi], zoom);
   },
 
+  createMarker: function(hub){
+    var icon;
+    hub.status === "ok" ? icon = this.greenMarker : icon = this.redMarker;
+    return L.marker([hub.latitude, hub.longitude], {icon:icon});
+  },
+
   renderMarkers:function(hubs){
     for (var i=0; i<hubs.length; i++){
-      if hubs[i]
-      var marker = L.marker([hubs[i].latitude, hubs[i].longitude], {icon:this.greenMarker})
-      popup = L.popup()
+     var marker = this.createMarker(hubs[i])
+      var popup = L.popup()
         .setContent('<p>'+ hubs[i].name +"<br /> Water price: $" + hubs[i].waterPrice + ".</p>")
       marker.bindPopup(popup).addTo(this.map);
       marker.on("click", marker.openPopup);
