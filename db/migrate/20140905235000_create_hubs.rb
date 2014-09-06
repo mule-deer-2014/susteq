4e50ed16e465845b14709d6f6f9eaa0ec7d68595
Ori <opleban@gmail.com>
2014-09-05 18:07:22 -0700

    adds constrains for hubs, requires location_id and type

--- db/migrate/20140905235000_create_hubs.rb
+++ db/migrate/20140905235000_create_hubs.rb
@@ -1,8 +1,8 @@
 class CreateHubs < ActiveRecord::Migration
   def change
     create_table :hubs do |t|
-      t.integer :location_id
-      t.string :type
+      t.integer :location_id, null:false
+      t.string :type, null:false
       t.string :provider_id
       t.decimal :longitude, {precision: 10, scale: 6}
       t.decimal :latitude, {precision: 10, scale: 6}
