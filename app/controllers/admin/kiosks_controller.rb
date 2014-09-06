d0cc518b526612aedfe53322fac10d5e2f887802
Eric Kennedy <ekeric13@gmail.com>
2014-09-06 11:56:07 -0700

    changed namespace of kiosk_controller

--- app/controllers/admin/kiosks_controller.rb
+++ app/controllers/admin/kiosks_controller.rb
@@ -1,48 +1,46 @@
-module Admin
-  class KiosksController < ApplicationController
+class Admin::KiosksController < ApplicationController
 
-    def show
-      @kiosk = Kiosk.find params[:id]
+  def show
+    @kiosk = Kiosk.find params[:id]
+  end
+
+  def create
+    @kiosk = Kiosk.new(kiosk_params)
+    @provider = Provider.find(params[:provider_id])
+    @provider.kiosks << @kiosk
+    if @kiosk.save
+      redirect_to kiosk_path(@kiosk)
+    else
+      render "kiosks/new"
     end
+  end
 
-    def create
-      @kiosk = Kiosk.new(kiosk_params)
-      @provider = Provider.find(params[:provider_id])
-      @provider.kiosks << @kiosk
-      if @kiosk.save
-        redirect_to kiosk_path(@kiosk)
-      else
-        render "kiosks/new"
-      end
+
+  def edit
+    @kiosk = Kiosk.find params[:id]
+  end
+
+  def update
+    @kiosk = Kiosk.find params[:id]
+    if @kiosk.update_attributes(kiosk_params)
+      redirect_to kiosk_path(@kiosk)
+    else
+      render "kiosks/edit"
     end
-
-
-    def edit
-      @kiosk = Kiosk.find params[:id]
-    end
-
-    def update
-      @kiosk = Kiosk.find params[:id]
-      if @kiosk.update_attributes(kiosk_params)
-        redirect_to kiosk_path(@kiosk)
-      else
-        render "kiosks/edit"
-      end
-
-    end
-
-    def destroy
-      Kiosk.destroy(params[:id])
-    end
-
-
-
-    private
-
-    def kiosk_params
-      params.require(:kiosk).permit(:longitude, :latitude, :location_id, :provider_id)
-    end
-
 
   end
+
+  def destroy
+    Kiosk.destroy(params[:id])
+  end
+
+
+
+  private
+
+  def kiosk_params
+    params.require(:kiosk).permit(:longitude, :latitude, :location_id, :provider_id)
+  end
+
+
 end
