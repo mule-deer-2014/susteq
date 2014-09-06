1a1c5ad3e81edc0e96f3bbd3e8caa35035f1ee22
Merissa McRissa <apprentice@DBC-Student-Personal.local>
2014-09-06 11:44:53 -0700

    modified admin/employee tests so can merge only good tests

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -15,42 +15,4 @@ describe Admin do
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
 
-  describe "when password is not present" do
-    before do
-      @admin = Admin.new(name: "Example admin", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
-    end
-    it { should_not be_valid }
-  end
-
-  describe "when password doesn't match confirmation" do
-    before do
-      @admin = Admin.new(name: "Example admin", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
-      @admin.password_confirmation = "mismatch"
-    end
-
-    it { should_not be_valid }
-  end
-
-  describe "return value of authenticate method" do
-  before do
-    @admin = Admin.create(name: "Example admin", email: "user@example.com",
-                     password: "bob", password_confirmation: "bob")
-  end
-  let(:found_admin) { Admin.find_by(email: @admin.email) }
-
-  describe "with valid password" do
-    it { should eq found_admin.authenticate(@admin.password) }
-  end
-
-  describe "with invalid password" do
-    let(:admin_for_invalid_password) { found_admin.authenticate("invalid") }
-
-    it { should_not eq admin_for_invalid_password }
-    specify { expect(admin_for_invalid_password).to be_false }
-  end
-end
-
-
 end
