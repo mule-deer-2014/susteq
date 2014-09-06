4dc9ce4704cb8ce521cf74d48384f5aa31b8a061
Ori <opleban@gmail.com>
2014-09-06 16:31:16 -0700

    adds empty provider tests, still need to fill in with assertions

---
+++ spec/controllers/providers_controller_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../rails_helper'
+
+describe ProvidersController do
+
+  describe 'Providers #show' do
+    it "responds successfully"
+
+    it "renders the :show template"
+
+    it "populates an array of hubs"
+
+  end
+
+end
