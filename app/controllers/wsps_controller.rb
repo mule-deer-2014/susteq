7e586456e7d00d3153b03878581ae292aa8ebaab
Igor Gaelzer <igor@nordweg.com>
2014-09-05 21:31:39 -0700

    Renamed WPSs to Providers across the system to match names with the rest of the group

--- app/controllers/wsps_controller.rb
+++
@@ -1,3 +0,0 @@
-class WspsController < ApplicationController
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
   resources :kiosks
-  resources :wsps
+  resources :providers
+  resources :employees
   
 
   # The priority is based upon order of creation: first created -> highest priority.
