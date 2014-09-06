0cc30b6ba1a92563e88045d675d5ca07bc2b4dcc
Ori <opleban@gmail.com>
2014-09-05 20:39:12 -0700

    Adds passing spec tests for admin, employee, hub and provider models * checks associations and attributes for each model

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../rails_helper'
+
+describe Admin do
+  #Tests that model has attributes
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:provider_id) }
+  it { should respond_to(:type) }
+  it { should respond_to(:phone_number) }
+
+  #Tests that has_secure_password rails method (that uses bcrypt) is working and encrypts password into digest
+  it { should respond_to(:password_hash) }
+  it { should respond_to(:remember_token) }
+
+
+end
---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../rails_helper'
+
+describe Employee do
+  #Tests that model has attributes
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:provider_id) }
+  it { should respond_to(:type) }
+  it { should respond_to(:phone_number) }
+
+  #Tests that has_secure_password rails method (that uses bcrypt) is working and encrypts password into digest
+  it { should respond_to(:password_hash) }
+  it { should respond_to(:remember_token) }
+
+  #Test associations
+  it { should belong_to(:provider) }
+end
