2618c39b59a451391d410a6d6a27599f242d9103
Whitney O'Banner <wobanner@gmail.com>
2014-09-06 15:12:01 -0700

    Add tests for Admin::Provider controller
    * Assert response status for GET routes
    * Create fake Provider object w FactoryGirl for testing

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-describe '' do
-  it ''
+describe Admin::ProvidersController do
+  before do
+    @provider = FactoryGirl.create(:provider)
+  end
+
+  describe 'Response statuses' do
+    describe 'GET providers#index' do
+      it 'returns a successful status' do
+        get :index
+        assert_response :success
+      end
+    end
+
+    describe 'GET providers#new' do
+      it 'returns a successful status' do
+        get :new
+        assert_response :success
+      end
+    end
+
+    describe 'GET providers#edit' do
+      it 'returns a successful status' do
+        get :edit, {id: @provider.id}
+        assert_response :success
+      end
+    end
+
+    describe 'GET providers#show' do
+      it 'returns a successful status' do
+        get :show, {id: @provider.id}
+        assert_response :success
+      end
+    end
+  end
 end
