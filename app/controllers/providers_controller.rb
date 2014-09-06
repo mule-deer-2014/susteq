e70fc8dd44f382967d307df1110a9c49205cbf0e
Whitney O'Banner <wobanner@gmail.com>
2014-09-05 17:14:22 -0700

    Namespace Providers controller
    * Wrap controller class in Admin module to trigger
      appropriate actions with nested routes

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,38 +1,40 @@
-class ProvidersController < ApplicationController
-  respond_to :html
+module Admin
+  class ProvidersController < ApplicationController
+    respond_to :html
 
-  def index
-    providers = Provider.all
-  end
+    def index
+      providers = Provider.all
+    end
 
-  def create
-    provider = Provider.create(provider_params)
-  end
+    def create
+      provider = Provider.create(provider_params)
+    end
 
-  def new
-    provider = Provider.new
-  end
+    def new
+      provider = Provider.new
+    end
 
-  def edit
-    provider = Provider.find(params[:id])
-  end
+    def edit
+      provider = Provider.find(params[:id])
+    end
 
-  def show
-    provider = Provider.find(params[:id])
-  end
+    def show
+      provider = Provider.find(params[:id])
+    end
 
-  def update
-    provider = Provider.find(params[:id])
-    provider.update(provider_params)
-  end
+    def update
+      provider = Provider.find(params[:id])
+      provider.update(provider_params)
+    end
 
-  def destroy
-    Provider.destroy(params[:id])
-  end
+    def destroy
+      Provider.destroy(params[:id])
+    end
 
-  private
-  
-  def provider_params
-    params.require(:provider).permit(:first_name, :last_name, :email, :password_hash)
+    private
+    
+    def provider_params
+      params.require(:provider).permit(:first_name, :last_name, :email, :password_hash)
+    end
   end
 end
