52c222222f3c393256b47c658693fd4cf92587b3
Whitney O'Banner <wobanner@gmail.com>
2014-09-06 00:29:43 -0700

    Add Employee controller with actions
    * Create skeleton CRUD methods

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,3 +1,26 @@
-class EmployeesController < ApplicationController
+module Admin
+  class EmployeesController < ApplicationController
+    respond_to :html
 
+    def index
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
 end
