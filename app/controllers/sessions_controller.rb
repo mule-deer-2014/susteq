2e74726e8cbf07ea70c312c3da2afcca040368e5
Merissa McRissa <apprentice@DBC-Student-Personal.local>
2014-09-05 17:38:21 -0700

    added namespacing for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,29 @@
-class SessionsController < ApplicationController
+
+class Admin::SessionsController < ApplicationController
+
+  #root '/' -> Renders new.html.erb which is login form
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      sessions[:remember_token] = @user.id
+      redirect_to @user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+
+end
+
+class Provider::SessionsController < ApplicationController
 
   #root '/' -> Renders new.html.erb which is login form
   def new
