a094b5d8c9a9dcb203d5767939ae4a43e0d3b34d
Alex Birdsall & Eric Kennedy <pair+ambirdsall.ekeric13@devbootcamp.com>
2014-09-05 17:04:51 -0700

    Creates 3 Providers with 4 Employees each with Faker data. NO DUNS NUMBER OR PASSWORDS YET.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,23 @@
     #create BCrypt password
   )
 end
+
+3.times do
+  Provider.create(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address
+    country: Faker::Address.country
+    #duns_number: (x..y).sample
+  )
+end
+
+Provider.all.each do |provider|
+  4.times do
+    provider.employees.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      #create BCrypt password
+    )
+  end
+end
