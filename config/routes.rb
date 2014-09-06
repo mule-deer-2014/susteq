a5c10e764825434d30d38df6a9f7e0e4a8624b74
Merissa McRissa <apprentice@DBC-Student-Personal.local>
2014-09-05 17:38:49 -0700

    revised routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  root  'sessions#new'
-  get '/signup',  to: 'users#new'
-  get '/signin',  to: 'sessions#new'
+
+  resources :providers do
+    resources :hubs
+  end
+
+  namespace :admin do
+    resources :providers, shallow: true do
+      resources :hubs
+    end
+  end
+  namespace :admin
+    resources :sessions, only: [:new, :create, :destroy]
+  end
+
+  namespace :employee
+    resources :sessions, only: [:new, :create, :destroy]
+  end
+
+  get '/', {:controller => "/employee/sessions", :action => "new"}
+  get '/admin', {:controller => "/admin/sessions", :action => "new"}
   delete '/signout', to: 'sessions#destroy'
 end
