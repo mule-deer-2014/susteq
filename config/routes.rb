e8a16ec3c58babf7837e601fc7adf3f6ffc224af
Merissa McRissa <apprentice@DBC-Student-Personal.local>
2014-09-05 21:49:26 -0700

    added updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,34 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
+  #ADMIN ROUTES
+  namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
+  end
+  get '/admin', to: "admin/sessions#new"
+  delete '/admin/signout', to: 'admin/sessions#destroy'
+
+  namespace :admin do
+    resources :kiosks
+    resources :pumps
+    resources :hubs
+    resources :providers do
+      resources :hubs
+    end
+  end
+
+  #EMPLOYEE ROUTES
+  namespace :employee do
+    resources :sessions, only: [:new, :create, :destroy]
+  end
+
+  get '/', to: "employee/sessions#new"
+  delete '/employee/signout', to: 'employee/sessions#destroy'
+
   resources :providers do
     resources :hubs
   end
 
-  namespace :admin do
-    resources :providers, shallow: true do
-      resources :hubs
-    end
-  end
-  namespace :admin
-    resources :sessions, only: [:new, :create, :destroy]
-  end
 
-  namespace :employee
-    resources :sessions, only: [:new, :create, :destroy]
-  end
 
-  get '/', {:controller => "/employee/sessions", :action => "new"}
-  get '/admin', {:controller => "/admin/sessions", :action => "new"}
-  delete '/signout', to: 'sessions#destroy'
 end
