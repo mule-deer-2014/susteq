4cc31c5cb57fb654e3e4cc3ae4592ebaecc595b6
Whitney O'Banner <wobanner@gmail.com>
2014-09-05 18:14:03 -0700

    Implement Provider controller actions
    * Access/modify Provider objects in CRUD methods
    * Whitelist modifiable params in private method
      `provider_params`

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,22 +1,38 @@
 class ProvidersController < ApplicationController
+  respond_to :html
+
   def index
+    providers = Provider.all
   end
 
   def create
+    provider = Provider.create(provider_params)
   end
 
   def new
+    provider = Provider.new
   end
 
   def edit
+    provider = Provider.find(params[:id])
   end
 
   def show
+    provider = Provider.find(params[:id])
   end
 
   def update
+    provider = Provider.find(params[:id])
+    provider.update(provider_params)
   end
 
   def destroy
+    Provider.destroy(params[:id])
+  end
+
+  private
+
+  def provider_params
+    params.require(:provider).permit(:first_name, :last_name, :email, :password_hash)
   end
 end
