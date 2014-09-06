9aa41727f0f1b28efe4becf60b6f28b925d8d65d
Alex Birdsall & Eric Kennedy <pair+ambirdsall.ekeric13@devbootcamp.com>
2014-09-05 17:00:37 -0700

    Creates 5 Admins with Faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  Admin.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    #create BCrypt password
+  )
+end
