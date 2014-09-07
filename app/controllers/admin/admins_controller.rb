8ac6404da5dfd1b954c7fd8c9ff8834405315628
Merissa McRissa <apprentice@DBC-Student-Personal.local>
2014-09-07 16:26:09 -0700

    added ability for admin to access and update profile page - still finishing update form post route

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -5,4 +5,16 @@ class Admin::AdminsController < ApplicationController
   def dashboard
     render 'admin/dashboard/dashboard'
   end
+
+  def show_current
+    render 'admin/admins/show_current'
+  end
+
+  def edit_current
+    render 'admin/admins/edit_current' #can make this form unhide rather than new pg in non-MVP
+  end
+
+  def update_current
+
+  end
 end
