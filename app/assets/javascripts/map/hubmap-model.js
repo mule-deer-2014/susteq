98d36fa905e19cf6126af7feade71b204ad57975
Ori <opleban@gmail.com>
2014-09-07 11:06:49 -0700

    modifies kiosk and pump models to reflect the most recent schema changes

--- app/assets/javascripts/map/hubmap-model.js
+++ app/assets/javascripts/map/hubmap-model.js
@@ -1,20 +1,22 @@
 function Kiosk(jsonData){
     this.id = jsonData.id;
+    this.type = "kiosk";
     this.name = jsonData.name;
     this.created_at = jsonData.created_at;
     this.latitude = jsonData.latitude;
     this.longitude = jsonData.longitude;
-    this.status = jsonData.status_code;
+    this.status_code = jsonData.status_code;
     this.location_id = jsonData.location_id;
 };
 
 function Pump(jsonData){
-    this.id = this.jsonData.id;
+    this.id = jsonData.id;
+    this.type = "pump";
     this.name = jsonData.name;
     this.created_at = jsonData.created_at;
     this.latitude = jsonData.latitude;
     this.longitude = jsonData.longitude;
-    this.status = jsonData.status_code;
+    this.status_code = jsonData.status_code;
     this.location_id = jsonData.location_id;
 };
 
