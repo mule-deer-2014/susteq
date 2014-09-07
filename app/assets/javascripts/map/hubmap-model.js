8c62643ada66ecf27e9a0a99bd72197ed9ab8f8e
Ori <opleban@gmail.com>
2014-09-06 23:41:48 -0700

    changes hubmap-model to reflect hubs table attributes

--- app/assets/javascripts/map/hubmap-model.js
+++ app/assets/javascripts/map/hubmap-model.js
@@ -1,40 +1,20 @@
-function Hub(jsonData){
-    this.id = jsonData.id
-    this.name = jsonData.name;
-    this.created_at = jsonData.created_at;
-    this.latitude = jsonData.latitude;
-    this.longitude = jsonData.longitude;
-    this.status = jsonData.status;
-    this.waterPrice = jsonData.waterPrice;
-};
-
 function Kiosk(jsonData){
     this.id = this.jsonData.id;
     this.name = jsonData.name;
     this.created_at = jsonData.created_at;
-    this.town = jsonData.town;
-    this.postalCode = jsonData.postalCode;
-    this.province = jsonData.province;
-    this.country = jsonData.country;
     this.latitude = jsonData.latitude;
     this.longitude = jsonData.longitude;
-    this.waterPrice = jsonData.waterPrice;
-    this.status = jsonData.status;
-    this.errors = jsonData.errors;
+    this.status = jsonData.status_code;
+    this.location_id = jsonDate.location_id;
 };
 
 function Pump(jsonData){
     this.id = this.jsonData.id;
     this.name = jsonData.name;
     this.created_at = jsonData.created_at;
-    this.town = jsonData.town;
-    this.postalCode = jsonData.postalCode;
-    this.province = jsonData.province;
-    this.country = jsonData.country;
     this.latitude = jsonData.latitude;
     this.longitude = jsonData.longitude;
-    this.waterPrice = jsonData.waterPrice;
-    this.status = jsonData.status;
-    this.errors = jsonData.errors;
+    this.status = jsonData.status_code;
+    this.location_id = jsonDate.location_id;
 };
 
