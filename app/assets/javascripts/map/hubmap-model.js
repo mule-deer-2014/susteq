d3cc52c6d8547f0226eaaabf90df63df4308d15f
Ori <opleban@gmail.com>
2014-09-07 00:47:31 -0700

    fixes syntax in kiosk model

--- app/assets/javascripts/map/hubmap-model.js
+++ app/assets/javascripts/map/hubmap-model.js
@@ -1,11 +1,11 @@
 function Kiosk(jsonData){
-    this.id = this.jsonData.id;
+    this.id = jsonData.id;
     this.name = jsonData.name;
     this.created_at = jsonData.created_at;
     this.latitude = jsonData.latitude;
     this.longitude = jsonData.longitude;
     this.status = jsonData.status_code;
-    this.location_id = jsonDate.location_id;
+    this.location_id = jsonData.location_id;
 };
 
 function Pump(jsonData){
