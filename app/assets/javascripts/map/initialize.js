1402c079be5d4947fe9b5a64d08b7cfa21e7893f
Ori <opleban@gmail.com>
2014-09-07 11:08:46 -0700

    per comments in pull request, assigns latitude and longitude data points to a constant

--- app/assets/javascripts/map/initialize.js
+++ app/assets/javascripts/map/initialize.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  var LAT_LONG_NAIROBI = [-1.283285, 36.821657];
   if ($("#map")){
-    var mapView = new HubMap.View(-1.283285, 36.821657, 13);
+    var mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
     mapView.setOSMTileLayer();
     var mapController = new HubMap.Controller(mapView);
     mapController.adminGetKioskData();
+    mapController.adminGetPumpData();
   }
 });
