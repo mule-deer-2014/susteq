a414b7f724d349862a601c170af720381204a620
Alex Birdsall <ambirdsall@gmail.com>
2014-09-06 22:37:31 -0700

    Pumps controller passes in Pumps from DB

--- app/controllers/pumps_controller.rb
+++ app/controllers/pumps_controller.rb
@@ -2,6 +2,10 @@ class PumpsController < ApplicationController
   layout "provider_application"
 
   def index
+    @pumps = Provider.find(params[:provider_id]).pumps
   end
 
+  def show
+    @pump = Pump.find(params[:id])
+  end
 end
