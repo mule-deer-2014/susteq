5856c9e8d2835d76a96af4d4d914f768160a7a4d
Whitney O'Banner <wobanner@gmail.com>
2014-09-06 18:02:30 -0700

    Add tests for Employees controller
    * Assert successful response statuses are returned
      from GET requests

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-describe '' do
-  it ''
+describe EmployeesController do
+  describe 'Response statuses' do
+    describe 'GET employees#index' do
+      it 'returns a successful status' do
+        get :index, provider_id: @employee.provider_id
+        assert_response :success
+      end
+    end
+
+    describe 'GET employees#new' do
+      it 'returns a successful status' do
+        get :new, provider_id: @employee.provider_id
+        assert_response :success
+      end
+    end
+
+    describe 'GET employees#edit' do
+      it 'returns a successful status' do
+        get :edit, {id: @employee.id, provider_id: @employee.provider_id}
+        assert_response :success
+      end
+    end
+
+    describe 'GET employees#show' do
+      it 'returns a successful status' do
+        get :show, {id: @employee.id, provider_id: @employee.provider_id}
+        assert_response :success
+      end
+    end
+  end
 end
