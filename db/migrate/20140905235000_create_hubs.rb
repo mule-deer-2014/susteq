12f4ad8132124b44c46cce999e759d37e91b3e58
Ori <opleban@gmail.com>
2014-09-06 22:52:22 -0700

    adds name field to hubs table

--- db/migrate/20140905235000_create_hubs.rb
+++ db/migrate/20140905235000_create_hubs.rb
@@ -1,12 +1,12 @@
 class CreateHubs < ActiveRecord::Migration
   def change
     create_table :hubs do |t|
-      t.string :name
       t.integer :location_id, null:false
       t.string :type, null:false
       t.string :provider_id
       t.decimal :longitude, {precision: 10, scale: 6}
       t.decimal :latitude, {precision: 10, scale: 6}
+      t.string :name
       t.timestamps
     end
   end
