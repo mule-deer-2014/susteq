51dcd36b58161238d4f57d2784c8ad273e267e96
Ori <opleban@gmail.com>
2014-09-06 23:46:58 -0700

    adds transaction model to hubmap-model.js

--- app/assets/javascripts/map/hubmap-model.js
+++ app/assets/javascripts/map/hubmap-model.js
@@ -18,3 +18,20 @@ function Pump(jsonData){
     this.location_id = jsonDate.location_id;
 };
 
+function Transaction(jsonData){
+    this.id = this.jsonDate.id;
+    this.transaction_time = jsonDate.transaction_time;
+    this.location_id = jsonDate.location_id;
+    this.latitude = jsonData.latitude;
+    this.longitude = jsonData.longitude;
+    this.rfid_id = jsonData.rfid_id;
+    this.starting_credits = jsonDate.starting_credits;
+    this.ending_credits = jsonDate.ending_credits;
+    this.transaction-code = jsonDate.transaction_code;
+    this.amount = jsonDate.amount;
+    this.error_code = jsonDate.error_code;
+    this.created_at = jsonDate.created_at;
+
+
+}
+
