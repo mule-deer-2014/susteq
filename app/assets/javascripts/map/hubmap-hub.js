f5611b1aac8d8c85134176964670f053f335d300
Ori <opleban@gmail.com>
2014-09-06 22:15:33 -0700

    adds kiosk model

--- app/assets/javascripts/map/hubmap-hub.js
+++ app/assets/javascripts/map/hubmap-hub.js
@@ -28,3 +28,18 @@ function Kiosk(jsonData){
     this.errors = jsonData.errors;
 };
 
+function Kiosk(jsonData){
+    this.id = this.jsonData.id;
+    this.name = jsonData.name;
+    this.created_at = jsonData.created_at;
+    this.town = jsonData.town;
+    this.postalCode = jsonData.postalCode;
+    this.province = jsonData.province;
+    this.country = jsonData.country;
+    this.latitude = jsonData.latitude;
+    this.longitude = jsonData.longitude;
+    this.waterPrice = jsonData.waterPrice;
+    this.status = jsonData.status;
+    this.errors = jsonData.errors;
+};
+
