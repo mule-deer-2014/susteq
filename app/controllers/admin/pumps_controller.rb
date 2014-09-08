87f311e011f6bef697c23cb359c111f874f6a6dd
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-08 02:56:11 -0700

    added logic to redirect pump flow appropriately based on if accessed from nested provider flow or top level flow; plus fixed a few errors left by teammates

--- app/controllers/admin/pumps_controller.rb
+++ app/controllers/admin/pumps_controller.rb
@@ -17,11 +17,13 @@ class Admin::PumpsController < ApplicationController
 
   def create
     @pump = Pump.new(pump_params)
-    # @providers = Provider.find(pump_params)
-    if @pump.save
-      redirect_to admin_pump_path(@pump)
+    if params[:provider_id]
+      provider = Provider.find(params[:provider_id])
+      redirect_to admin_provider_path(provider) and return if @pump.save
+      redirect_to new_admin_provider_pump_path(provider)
     else
-      render "admin/pumps/new"
+      redirect_to admin_pumps_path and return if @pump.save
+      redirect_to new_admin_pump_path
     end
   end
 
@@ -29,20 +31,32 @@ class Admin::PumpsController < ApplicationController
     @pump = Pump.find params[:id]
   end
 
-  def update
-    @pump = Pump.find params[:id]
-    if @pump.update_attributes(pump_params)
-      redirect_to admin_pump_path(@pump)
+  def update #MLM NOTES TO REFACTOR - Need different way to redirect user when approach from nested provider route vs top level route
+    pump = Pump.find(params[:id])
+    if params[:provider_id]
+      provider = Provider.find(params[:provider_id])
+      if pump.update(pump_params)
+        redirect_to admin_provider_path(provider)
+      else
+        redirect_to edit_admin_provider_pump_path(provider,pump)
+      end
     else
-      render "admin/pumps/edit"
+      if pump.update(pump_params)
+        redirect_to admin_pumps_path
+      else
+        redirect_to edit_admin_pump_path(pump)
+      end
     end
-
   end
 
   def destroy
-    provider = Provider.find( (Pump.find(params[:id])).provider_id )
     Pump.destroy(params[:id])
-    redirect_to admin_pumps_path
+    if params[:provider_id]
+      @provider = Provider.find(params[:provider_id])
+      redirect_to admin_provider_path(@provider)
+    else
+      redirect_to admin_pumps_path
+    end
   end
 
 
@@ -50,7 +64,7 @@ class Admin::PumpsController < ApplicationController
   private
 
   def pump_params
-    params.require(:pump).permit(:longitude, :latitude, :location_id, :provider_id)
+    params.require(:pump).permit(:name,:longitude, :latitude, :location_id, :provider_id)
   end
 
 end
