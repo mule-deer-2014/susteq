8c06469741334cb0d66af48c78cf30cd6ba40b28
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-07 23:21:05 -0700

    replaced Provider.find with current_rpovider

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   before_filter :require_employee_signin
 
   def dashboard
-    @hubs = Provider.find(current_provider).hubs
+    @hubs = current_provider.hubs
     render "dashboard/dashboard"
   end
 end
