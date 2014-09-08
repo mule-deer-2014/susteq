5fa794d3a5ad97e1bd302b919f7a67b5440246da
Ori <opleban@gmail.com>
2014-09-08 06:08:55 -0700

    changes js initialize to check cookies first to see if user is logged in

--- app/assets/javascripts/big-data/initialize.js
+++ app/assets/javascripts/big-data/initialize.js
@@ -2,8 +2,20 @@ var LAT_LONG_NAIROBI = [-1.283285, 36.821657];
 
 $(document).ready(function(){
   var dataController = new BigData.DataController();
-  dataController.mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
-  dataController.getAdminData(function(){
-    this.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
-  }.bind(dataController));
+  if (docCookies.getItem("remember_token")){
+    dataController.checkPermissions(function(data){
+      if ($('#map').length > 0)
+        dataController.mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
+      if (data.permission === "admin"){
+        dataController.getAdminData(function(){
+          this.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
+        }.bind(dataController));
+      }
+      else{
+        dataController.getProviderData(data.permission, function(){
+          this.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
+          }.bind(dataController));
+        }
+    });
+  }
 });
