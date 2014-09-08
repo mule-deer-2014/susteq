ccbdf532e21d9854ff6aae262c77e94f6b1eae24
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-07 23:04:26 -0700

    edited employee controller to work with now unnested routes and removed unneccary actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,50 +1,45 @@
 class EmployeesController < ApplicationController
+  layout "provider_application"
   respond_to :html
   before_filter :require_employee_signin
 
   def index
-    @employees = Employee.all
+    @employees = current_provider.employees.all
   end
 
   def create
-    @employee = Employee.new(employee_params)
+    @employee = current_provider.employees.new(employee_params)
 
-    redirect_to provider_employees_path and return if @employee.save
-    redirect_to new_provider_employee_path
+    redirect_to employees_path and return if @employee.save
+    redirect_to new_employee_path
   end
 
   def new
     @employee = Employee.new
-    @provider = Provider.find(params[:provider_id])
   end
 
   def edit
     @employee = Employee.find(params[:id])
   end
 
-  def show
-    @employee = Employee.find(params[:id])
-  end
-
   def update
     employee = Employee.find(params[:id])
 
-    if employee.update(employee_params) 
-      redirect_to provider_employee_path(employee.provider_id, employee) 
+    if employee.update(employee_params)
+      redirect_to employees_path
     elsif employee_params.fetch(:password, []).empty?
-      employee.update_attribute(:provider_id, employee_params[:name])
       employee.update_attribute(:name, employee_params[:name])
-      employee.update_attribute(:email, employee_params[:name])
-      employee.update_attribute(:phone_number, employee_params[:name])
-      redirect_to provider_employee_path(employee.provider_id, employee) 
+      employee.update_attribute(:email, employee_params[:email])
+      employee.update_attribute(:phone_number, employee_params[:phone_number])
+      redirect_to employees_path
     else
-      redirect_to edit_provider_employee_path(employee.provider_id, employee)
+      redirect_to edit_employee_path(employee)
     end
   end
 
   def destroy
     Employee.destroy(params[:id])
-    redirect_to provider_employees_path
+    redirect_to employees_path
   end
 
   private
