9cabaf59eacf130e60b6b38a6bbebf1a92017171
Ori <opleban@gmail.com>
2014-09-07 18:50:53 -0700

    Grabs all kiosks from admin kiosk controller with transactions. * adds getAdminKioskData which makes the ajax call * adds parseJsonKioskData which calls the Kiosk constructor function

--- app/assets/javascripts/bigdata-controller.js
+++ app/assets/javascripts/bigdata-controller.js
@@ -1,5 +1,27 @@
 BigData.Controller = function(){
   this.kiosks = [];
   this.pumps = [];
-
 };
+
+BigData.prototype = {
+
+  adminGetKioskData: function(){
+    var kioskAjax = $.ajax({
+      url:"/admin/kiosks.json",
+      method:"get"
+    }).
+    done(function(data){
+      this.parseJsonKioskData(data.kiosks);
+      this.view.renderMarkers(this.kiosks);
+    }.bind(this)).
+    fail();
+  },
+
+  parseJsonKioskData: function(kioskData){
+    for(var i= 0; i<kioskData.length; i++){
+      var kiosk = new Kiosk(kioskData[i]);
+      this.kiosks.push(kiosk);
+    }
+  },
+
+}
