f92c132f9ff431b84e31414a669a25181c518aa2
Ori <opleban@gmail.com>
2014-09-07 19:51:12 -0700

    Modifies methods to correctly parse kiosk data from backend *parseJsonKioskData creates kiosk object with kiosk json data and then calls a kiosk function which parses the transaction json data and assigns to the kiosk object as an array of transaction objects.
    *Similar methods created for Pump

--- app/assets/javascripts/bigdata-controller.js
+++ app/assets/javascripts/bigdata-controller.js
@@ -12,16 +12,34 @@ BigData.prototype = {
     }).
     done(function(data){
       this.parseJsonKioskData(data.kiosks);
-      this.view.renderMarkers(this.kiosks);
     }.bind(this)).
     fail();
   },
 
   parseJsonKioskData: function(kioskData){
     for(var i= 0; i<kioskData.length; i++){
-      var kiosk = new Kiosk(kioskData[i]);
+      var kiosk = new Kiosk(kioskData[i].hub);
+      kiosk.parseTransactions(kioskData[i].transactions)
       this.kiosks.push(kiosk);
     }
   },
 
+  adminGetPumpData: function(){
+    var pumpAjax = $.ajax({
+      url:"/admin/pumps.json",
+      method:"get"
+    }).
+    done(function(data){
+      this.parseJsonPumpData(data.kiosks);
+    }.bind(this)).
+    fail();
+  },
+
+  parseJsonPumpData: function(pumpData){
+    for(var i= 0; i<pumpData.length; i++){
+      var pump = new Pump(pumpData[i].hub);
+      pump.parseTransactions(pumpData[i].transactions)
+      this.pumps.push(pump);
+    }
+  },
 }
