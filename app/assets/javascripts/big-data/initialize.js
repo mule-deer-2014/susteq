b6a9b7a5bd1c83ce2264a0d7221a051527376eca
Ori <opleban@gmail.com>
2014-09-08 10:15:13 -0700

    adds map view conditional statement to provder ajax request as well in initialize method

--- app/assets/javascripts/big-data/initialize.js
+++ app/assets/javascripts/big-data/initialize.js
@@ -4,18 +4,22 @@ $(document).ready(function(){
   var dataController = new BigData.DataController();
   if (docCookies.getItem("remember_token")){
     dataController.checkPermissions(function(data){
-      if ($('#map').length > 0)
-        dataController.mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
       if (data.permission === "admin"){
         dataController.getAdminData(function(){
-          this.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
+          if ($('#map').length > 0){
+            this.mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
+            this.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
+          }
           this.grapher.makeDataForHubs(this.allHubs());
           this.grapher.makeCharts();
         }.bind(dataController));
       }
       else{
         dataController.getProviderData(function(){
-          this.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
+          if ($('#map').length > 0){
+            this.mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
+            this.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
+          }
         }.bind(dataController));
       }
     });
