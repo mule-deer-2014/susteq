4a7ce57750f1f37ab48ce7d4218eccc715098944
Ori <opleban@gmail.com>
2014-09-07 18:55:43 -0700

    Adds class methods to hub which are inherited by pump and kiosk * adds get_with_transactions, an instance method which grabs self and transactions and returns an object with both * adds self method which calls get_with_transactions on all hubs

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -2,4 +2,12 @@ class Hub < ActiveRecord::Base
   has_many :transactions
 
   validates :location_id, uniqueness: true
+
+  def get_with_transactions
+    {hub: self, transactions: transactions}
+  end
+
+  def self.get_all_with_transactions
+    all.map{ |hub| hub.get_with_transactions }
+  end
 end
