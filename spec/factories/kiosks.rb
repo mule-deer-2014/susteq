73a6994a37b545e1bb832e7b07d84c9fc8cf03c2
Whitney O'Banner <wobanner@gmail.com>
2014-09-08 12:14:08 -0700

    Update factories with required data
    * Add password attribute to admin factory
    * Add all required attributes to kiosk factory
    * Store provider ID of existing provider as attribute for employee

--- spec/factories/kiosks.rb
+++ spec/factories/kiosks.rb
@@ -2,5 +2,8 @@ require 'faker'
 
 FactoryGirl.define do
   factory :kiosk do
+    name {Faker::Lorem.word}
+    location_id {Faker::Number.number(2)}
+    provider {FactoryGirl.create(:provider)}
   end
 end
