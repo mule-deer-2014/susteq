43446b8b7683f6e26220e317ec38d5e1a8c60cf9
Ori <opleban@gmail.com>
2014-09-07 21:21:09 -0700

    comments out map driver code

--- app/assets/javascripts/map/initialize.js
+++ app/assets/javascripts/map/initialize.js
@@ -1,10 +1,8 @@
-$(document).ready(function() {
-  var LAT_LONG_NAIROBI = [-1.283285, 36.821657];
-  if ($("#map")){
-    var mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
-    mapView.setOSMTileLayer();
-    var mapController = new HubMap.Controller(mapView);
-    mapController.adminGetKioskData();
-    mapController.adminGetPumpData();
-  }
-});
+// $(document).ready(function() {
+//   var LAT_LONG_NAIROBI = [-1.283285, 36.821657];
+//   if ($("#map")){
+//     var mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
+//     mapView.setOSMTileLayer();
+//     dataController.mapController = new HubMap.Controller(mapView);
+//   }
+// });
