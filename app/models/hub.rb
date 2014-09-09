a2423538ca2597a0453cabd7723266cb014d712c
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-09 02:44:45 -0700

    added validation to Hub model

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -1,7 +1,11 @@
 class Hub < ActiveRecord::Base
   has_many :transactions
 
-  validates :location_id, uniqueness: true
+  validates :location_id, uniqueness: true, numericality: { only_integer: true}
+  validates :location_id, presence: true
+  validates :latitude, numericality: { only_integer: true, :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90}
+  validates :longitude, numericality: { only_integer: true, :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180}
+  validates :status_code, uniqueness: true, numericality: { only_integer: true}
 
   def get_with_transactions
     {hub: self, transactions: transactions}
