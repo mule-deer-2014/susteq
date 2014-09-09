003689a6e5569ee60c38125f241226acd5638370
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-08 22:46:49 -0700

    reset js files back inline with master

--- app/assets/javascripts/chart/chartMaker.js
+++ app/assets/javascripts/chart/chartMaker.js
@@ -1,34 +1,3 @@
-var CGraphs = {
-  $creditsSoldByKiosk = $("#credits-sold-by-kiosk")
-}
-
-HubChart.BarChartMaker = function(object) {
-  this.dataSet = object.dataSet;
-  this.dom = object.dom;
-  this.title = object.title;
-}
-
-HubChart.BarChartMaker.prototype = function(object) {
-  makeBarChart: function() {
-    polyjs.chart({
-      layer: {
-        data: this.dataSet,
-        type: "bar",
-        x: "bin(date, 'month')",
-        y: "sum(dispensed)"
-      },
-      guide: {
-        x: { title: "Month"},
-        y: { title: ""}
-      },
-        title: this.title,
-        dom: this.dom,
-        width: 500,
-        height: 250
-    });
-  }
-}
-
 HubChart.ChartMaker = function() {
   this.kioskDiv = document.getElementById("kiosk-charts");
   this.pumpDiv = document.getElementById("pump-charts");
@@ -115,45 +84,22 @@ HubChart.ChartMaker.prototype = {
   });
   },
 
-
   makePumpChart: function(dataSet, chartElement) {
     polyjs.chart({
-      layer: {
-        data: dataSet,
-        type: "bar",
-        x: "bin(date, 'month')",
-        y: "sum(dispensed)"
-      },
-      guide: {
-        x: { title: "Month"},
-        y: { title: ""}
-      },
-        title: "Liters of Water Dispensed by Month",
-        dom: chartElement,
-        width: 500,
-        height: 250
-    });
-  },
-
-  get_credits_sold_by_kiosk: function(){
-    var controller = this;
-    if (CGraphs.$creditsSoldByKiosk.length > 0){
-      var creditsKiosksAjax = $.get("/transactions/credits_sold_by_kiosk.json")
-                                .done(function(data){
-                                  var x_array = data.x
-                                  var y_array = data.y
-                                  var dataSet = polyjs.data({
-                                    date: dates,
-                                    credits: amounts
-                                  });
-
-                                  objct = {
-                                    dataSet:
-                                    title:
-                                  }
-                                  controller.makeKioskChart(object, CGraphs.$creditsSoldByKiosk)
-                                })
-    }
-
+    layer: {
+      data: dataSet,
+      type: "bar",
+      x: "bin(date, 'month')",
+      y: "sum(dispensed)"
+    },
+    guide: {
+      x: { title: "Month"},
+      y: { title: ""}
+    },
+    title: "Liters of Water Dispensed by Month",
+    dom: chartElement,
+    width: 500,
+    height: 250
+  });
   }
 };
