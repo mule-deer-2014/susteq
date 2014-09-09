8da19438c9cd3a5d4cf893f44d43507d1a99d93f
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-09 04:16:21 -0700

    flanged out views for pumps and koisks on provider dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #ROUTES FOR PROVIDER DASHBOARD
   get '/employee', to: "employee/sessions#new", as: 'employee_signin'
   get '/employee/signout', to: 'employee/sessions#destroy', as: 'employee_signout' #get rather than delete bc of issue with twitter bootstrap link_to
-  get '/dashboard', to:"dashboard#dashboard", as: "provider_dashboard"
+  get '/dashboard', to:"dashboard#main", as: "provider_dashboard"
   get '/sessions', to: "sessions#index"
   get '/my_profile', to: 'employees#show_current', as: 'current_employee'
   get '/edit_profile', to: 'employees#edit_current', as: 'edit_current_employee'
@@ -25,7 +25,8 @@ Rails.application.routes.draw do
   #ROUTES FOR ADMIN DASHBOARD
   get '/admin', to: "admin/sessions#new", as: 'admin_signin'
   get '/admin/signout', to: 'admin/sessions#destroy', as: 'admin_signout' #get rather than delete bc of issue with twitter bootstrap link_to
-  get '/admin/dashboard', to:'admin/admins#dashboard', as: 'admin_dashboard'
+  get '/admin/dashboard', to:'admin/dashboard#main', as: 'admin_dashboard'
+  get '/admin/operations', to:'admin/dashboard#operations', as: 'admin_operations'
   get '/admin/my_profile', to: 'admin/admins#show_current', as: 'current_admin'
   get '/admin/edit_profile', to: 'admin/admins#edit_current', as: 'edit_current_admin'
   post '/admin/my_profile', to: 'admin/admins#update_current', as: 'update_current_admin' #post rather than put bc of issue with twitter bootstrap link_to
