8b7fb14b94f43bb702af66a89884b337eb55602e
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-09 02:42:10 -0700

    added hard validation check and rescue error functionality to kiosk controller

--- app/controllers/admin/kiosks_controller.rb
+++ app/controllers/admin/kiosks_controller.rb
@@ -20,14 +20,23 @@ class Admin::KiosksController < ApplicationController
   end
 
   def create
-    @kiosk = Kiosk.new(kiosk_params)
     if params[:provider_id]
+      begin
       provider = Provider.find(params[:provider_id])
-      redirect_to admin_provider_path(provider) and return if @kiosk.save
-      redirect_to new_admin_provider_kiosk_path(provider)
+      @kiosk = Kiosk.create!(kiosk_params)
+      redirect_to admin_provider_path(provider)
+      rescue ActiveRecord::RecordInvalid => invalid
+        flash[:error_messages] = invalid.record.errors.full_messages
+        redirect_to new_admin_provider_kiosk_path(provider)
+      end
     else
-      redirect_to admin_kiosks_path and return if @kiosk.save
-      redirect_to new_admin_kiosk_path
+      begin
+      @kiosk = Kiosk.create!(kiosk_params)
+      redirect_to admin_kiosks_path
+      rescue ActiveRecord::RecordInvalid => invalid
+        flash[:error_messages] = invalid.record.errors.full_messages
+        redirect_to new_admin_kiosk_path
+      end
     end
   end
 
@@ -38,16 +47,20 @@ class Admin::KiosksController < ApplicationController
   def update #MLM NOTES TO REFACTOR - Need different way to redirect user when approach from nested provider route vs top level route
     kiosk = Kiosk.find(params[:id])
     if params[:provider_id]
+      begin
       provider = Provider.find(params[:provider_id])
-      if kiosk.update(kiosk_params)
-        redirect_to admin_provider_path(provider)
-      else
+      kiosk.update!(kiosk_params)
+      redirect_to admin_provider_path(provider)
+      rescue ActiveRecord::RecordInvalid => invalid
+        flash[:error_messages] = invalid.record.errors.full_messages
         redirect_to edit_admin_provider_kiosk_path(provider,kiosk)
       end
     else
-      if kiosk.update(kiosk_params)
-        redirect_to admin_kiosks_path
-      else
+      begin
+      kiosk.update!(kiosk_params)
+      redirect_to admin_kiosks_path
+      rescue ActiveRecord::RecordInvalid => invalid
+        flash[:error_messages] = invalid.record.errors.full_messages
         redirect_to edit_admin_kiosk_path(kiosk)
       end
     end
@@ -65,6 +78,6 @@ class Admin::KiosksController < ApplicationController
 
   private
   def kiosk_params
-    params.require(:kiosk).permit(:name,:longitude, :latitude, :location_id, :provider_id)
+    params.require(:kiosk).permit(:name,:longitude, :latitude, :location_id, :provider_id,:status_code)
   end
 end
