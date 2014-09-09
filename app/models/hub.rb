81fb5d3e2f170eae10779b0395370a75ee657fca
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-09 16:42:55 -0700

    added/edited validations to hub and user models

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -1,11 +1,11 @@
 class Hub < ActiveRecord::Base
   has_many :transactions
 
-  validates :location_id, uniqueness: true, numericality: { only_integer: true}
-  validates :location_id, presence: true
+  validates :name, length: { maximum: 50 }
+  validates :location_id, presence: true, uniqueness: true, numericality: { only_integer: true}
   validates :latitude, numericality: { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90}, allow_nil: true
   validates :longitude, numericality: { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180}, allow_nil: true
-  validates :status_code, numericality: { only_integer: true}, allow_nil: true
+  validates :status_code, numericality: { only_integer: true}
 
   def get_with_transactions
     {hub: self, transactions: transactions}
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ActiveRecord::Base
   belongs_to :provider
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  ONLY_NUMBERS = /\A[0-9]+\z/
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  validates :phone_number, format: { with: ONLY_NUMBERS, message:"should only have numeric values." }, allow_nil: true
 
   has_secure_password
   before_create :create_remember_token
