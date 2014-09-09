0190c5fe269d5092633f7228ef8a794792cddb90
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-09 06:02:44 -0700

    trying to removevalidations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   belongs_to :provider
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # before_save { self.email = email.downcase }
+  # validates :name, presence: true, length: { maximum: 50 }
+  # validates :email, presence:   true,
+  #                   format:     { with: VALID_EMAIL_REGEX },
+  #                   uniqueness: { case_sensitive: false }
+  # validates :password, length: { minimum: 6 }
 
   has_secure_password
   before_create :create_remember_token
