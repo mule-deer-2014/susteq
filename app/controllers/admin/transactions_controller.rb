cfc2cc667e774af3d0eb49c4afc386c1251f9b5e
Ori <opleban@gmail.com>
2014-09-09 19:35:23 -0700

    spike branch

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -1,5 +1,6 @@
 class TransactionsController < ApplicationController
   scope :created_before, ->(time) { where("created_at < ?", time) }
+  scope :credits_sold, ->{where(transaction_type)}
 
   def most_recent_transactions
     Transaction.find(:all, :order => "transaction_time desc", :limit => 10).reverse
