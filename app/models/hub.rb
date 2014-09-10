ff550aecff4ac743deed031515a8ac9e14cb6d20
Alex Birdsall <ambirdsall@gmail.com>
2014-09-09 17:27:52 -0700

    Adds validation to Hub model for existence of type.

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -1,6 +1,7 @@
 class Hub < ActiveRecord::Base
   has_many :transactions
 
+  validates :type, presence: true
   validates :location_id, uniqueness: true, numericality: { only_integer: true}
   validates :location_id, presence: true
   validates :latitude, numericality: { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90}
