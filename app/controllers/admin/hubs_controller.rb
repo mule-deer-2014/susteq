e760a3ff482b7334f92e2e2dda50361b545e80ca
Alex Birdsall <ambirdsall@gmail.com>
2014-09-09 17:01:07 -0700

    New hub creation for unassigned transactions now renders a single hub form with type select field.

--- app/controllers/admin/hubs_controller.rb
+++ app/controllers/admin/hubs_controller.rb
@@ -10,7 +10,32 @@ class Admin::HubsController < ApplicationController
   def add_new_hub
     @new_location_id = params[:new_hubs_id]
     @providers = Provider.all
-    @kiosk = Kiosk.new(location_id: @new_location_id)
-    @pump = Pump.new(location_id: @new_location_id)
+    @hub = Hub.new(location_id: @new_location_id)
+  end
+
+  def create
+    if params[:provider_id]
+      begin
+      @provider = Provider.find(params[:provider_id])
+      @kiosk = Hub.create!(hub_params)
+      redirect_to admin_provider_path(@provider)
+      rescue ActiveRecord::RecordInvalid => invalid
+        flash[:error_messages] = invalid.record.errors.full_messages
+        redirect_to new_admin_provider_hub_path(@provider)
+      end
+    else
+      begin
+      @hub = Hub.create!(hub_params)
+      redirect_to admin_dashboard_path
+      rescue ActiveRecord::RecordInvalid => invalid
+        flash[:error_messages] = invalid.record.errors.full_messages
+        redirect_to new_admin_hub_path
+      end
+    end
+  end
+
+  private
+  def hub_params
+    params.require(:hub).permit(:type,:name,:longitude, :latitude, :location_id, :provider_id,:status_code)
   end
 end
