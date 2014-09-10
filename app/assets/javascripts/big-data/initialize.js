717190529b6fd7322a61e06e10c51ff35c19e25b
Ori <opleban@gmail.com>
2014-09-10 13:10:14 -0700

    Refactors initalize.js driver code. * Removes all ajax calls, inserts new getChartData function and stub data

--- app/assets/javascripts/big-data/initialize.js
+++ app/assets/javascripts/big-data/initialize.js
@@ -1,16 +1,13 @@
-var LAT_LONG_NAIROBI = [-1.283285, 36.821657];
+// var LAT_LONG_NAIROBI = [-1.283285, 36.821657];
+var dataToDisplay = [{xAxisTitle: "Kiosk Location Id",
+                      yAxisTitle: "Credits Sold",
+                      chartData: [{location_id:14, total:2000},
+                                {location_id: 11, total:1400},
+                                {location_id: 4, total: 2300},
+                                {location_id: 7, total:300}],
+                      chartType: "bar",
+                      divSelector:"#total-credits-sold"}];
 $(document).ready(function(){
   var dataController = new BigData.DataController();
-  if (docCookies.getItem("remember_token")){
-    dataController.checkPermissions(function(data){
-      if (data.permission === "admin"){
-        this.getAdminHubData(this.generateMap.bind(this));
-        HubChart.totalCreditsSold.createChart();
-      }
-      else{
-        this.getProviderHubData(this.generateMap).bind(this);
-        generateProviderCharts();
-        }
-      }.bind(dataController));
-  };
+    dataController.getChartData(dataToDisplay);
 });
