e8dd5b02993c1d3ece77d3330bd6f044a333771b
Whitney O'Banner <wobanner@gmail.com>
2014-09-10 14:18:21 -0700

    Update employee factory to pass validations
    * New validation requires phone number to can only contain digits
    * Change phone number attribute in employee factory from Faker::PhoneNumber
      to Faker::Number with 10 digits

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     provider_id {FactoryGirl.create(:provider).id}
     name {Faker::Name.name}
     email {Faker::Internet.email}
-    phone_number {Faker::PhoneNumber.phone_number}
+    phone_number {Faker::Number.number(10)}
 
     trait :with_password do
       password "1234567"
