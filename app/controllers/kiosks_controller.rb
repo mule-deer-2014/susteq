1a079fbabb27e890342b57a2b44a8a5051619b67
Igor Gaelzer <igor@nordweg.com>
2014-09-11 15:32:58 -0700

    Merges changes into kiosk index view

--- app/controllers/kiosks_controller.rb
+++ app/controllers/kiosks_controller.rb
@@ -5,17 +5,7 @@ class KiosksController < ApplicationController
 
   def index
     @kiosks = current_provider.kiosks
-<<<<<<< HEAD
-    sum = 0
-    @kiosks.each do |k|
-      sum += k.credits_sold
-    end
-    @total_credits_sold = sum
-    hubs = getHubs
-    @viz_data = [hubs].to_json
-=======
     @viz_data = [credits_by_kiosk_for_provider(current_provider), getHubs].to_json
->>>>>>> origin/new-in-process-master
     @total_credits_sold = @kiosks.reduce(0) { |sum, kiosk| sum + kiosk.credits_sold }
   end
 
