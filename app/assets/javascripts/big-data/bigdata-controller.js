824dbc16b54127f5f9f6f7e3e7ac37e7ae515344
Ori <opleban@gmail.com>
2014-09-10 18:06:49 -0700

    properly names sub functions in createHubs in DataController

--- app/assets/javascripts/big-data/bigdata-controller.js
+++ app/assets/javascripts/big-data/bigdata-controller.js
@@ -13,7 +13,7 @@ BigData.DataController.prototype = {
           that.createBarGraph(index, data);
           break;
         case "map":
-          that.createMap(index, data);
+          that.createMap(index, data.chartData);
           break;
       }
     });
@@ -39,25 +39,26 @@ BigData.DataController.prototype = {
     that.createHubs(data)
     var LAT_LONG_NAIROBI = [-1.283285, 36.821657];
     that.mapView = new HubMap.View(LAT_LONG_NAIROBI[0], LAT_LONG_NAIROBI[1], 11);
+    that.mapView.displayAllHubs({kiosks:this.kiosks, pumps:this.pumps});
   },
 
   createHubs: function(data){
-    $.each(data.chartData.kiosks, this.parseJsonKiosks)
-    $.each(data.chartData.pumps, this.parseJsonPumps)
+    $.each(data.kiosks, this.parseJsonKioskData)
+    $.each(data.pumps, this.parseJsonPumpData)
   },
 
   parseJsonKioskData: function(index, kioskData){
     $.each(kioskData, function(index, kioskDatum){
       var kiosk = new Kiosk(kioskDatum);
       this.kiosks.push(kiosk);
-    }
+    })
   },
 
   parseJsonPumpData: function(index, pumpData){
     $.each(pumpData, function(index, pumpDatum){
       var pump = new Pump(pumpDatum);
       this.pumps.push(pump);
-    }
+    })
   },
 
 };
