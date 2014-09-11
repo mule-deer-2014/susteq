0c525b0843212c11a15fa53f6136af5ebcb59928
Eric Kennedy <ekeric13@gmail.com>
2014-09-11 12:35:41 -0700

    cleaned merge conflict

--- app/controllers/admin/operations_controller.rb
+++ app/controllers/admin/operations_controller.rb
@@ -2,6 +2,7 @@ class Admin::OperationsController < ApplicationController
 	layout "admin_application"
 
 	def index
+    @something = method
 		@transactions = Transaction.all
   	@total_amount_transactions_by_month = Transaction.select("extract(month from transaction_time) as name, sum(amount) as total" ).where("transaction_code = 20 or transaction_code = 21").group("name").order("name DESC")
     @new_hubs_ids = (Transaction.all - Hub.get_all_transactions)
@@ -10,4 +11,5 @@ class Admin::OperationsController < ApplicationController
     hubs = getHubs
     @viz_data = [hubs].to_json
   end
+
 end
