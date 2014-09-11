541a5816726676407ec6c68df28d938cc8013ead
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-10 21:14:23 -0700

    added credit sold  by month method

--- app/helpers/perspective_summary.rb
+++ app/helpers/perspective_summary.rb
@@ -1,45 +1,42 @@
 module PerspectiveSummary
-  def credits_by_kiosk
+  def credits_by_kiosk_by_month
     chart_data_array = []
     #Query for Stacked Bar Chart
-    @month_by_kiosk_total_obj_arr = Transaction.select("location_id, sum(amount) as total,extract(month from transaction_time) as month").where("transaction_code = 20 or transaction_code = 21").group("extract(month from transaction_time),location_id")
-    #Query for Bar Chart and Table
-    @kiosk_total_obj_arr = Transaction.select("location_id, sum(amount) as total").where("transaction_code = 20 or transaction_code = 21").group("location_id").order("sum(amount)")
+    month_by_kiosk_total_obj_arr = Transaction.select("location_id, sum(amount) as total,extract(month from transaction_time) as month").where("transaction_code = 20 or transaction_code = 21").group("extract(month from transaction_time),location_id")
 
     stacked_data_to_display = []
     (Date.today.month-5..Date.today.month).each do |month|
       month_hash[:month] = month
-      @month_by_kiosk_total_obj_arr.select{|obj| obj.month == month }.each do |obj|
+      month_by_kiosk_total_obj_arr.select{|obj| obj.month == month }.each do |obj|
         location_key = "location_id".concatenate(obj.location_id.to_s).to_sym
         month_hash[location_key] = obj.total
       end
       stacked_data_to_display.push(month_hash)
     end
 
+  end
+
+  def credits_by_kiosk
+    chart_data_array = []
+    #Query for Bar Chart and Table
+    kiosk_total_obj_arr = Transaction.select("location_id, sum(amount) as total").where("transaction_code = 20 or transaction_code = 21").group("location_id").order("sum(amount)")
     #Prepare data for Normalchart
-    @kiosk_total_obj_arr.each do |obj|
+    kiosk_total_obj_arr.each do |obj|
       kiosk_obj = {location_id: obj.location_id, total: obj.total}
       chart_data_array.push(kiosk_hash)
     end
-
-    #Create chart obj
-    data_to_display = {
-    xAxisTitle: "Kiosk Location Id",
-    yAxisTitle: "Credits Sold",
-    chartData: chart_data_array,
-    chartType: "bar"
-    };
-    #Return as json obj
+    #Create json chart obj
+    data_to_display = { xAxisTitle: "Kiosk Location Id", yAxisTitle: "Credits Sold", chartData: chart_data_array, chartType: "bar"};
     @viz_data = data_to_display.to_json
   end
 
   def dispensed_by_month(pump)
     #Query db
-    @dispensed_by_month = Transaction.select("sum(amount) as total,extract(month from transaction_time) as month").where("transaction_code = 1 and location_id = #{pump.location_id}").group("extract(month from transaction_time)")
+    dispensed_by_month = Transaction.select("sum(amount) as total,extract(month from transaction_time) as month").where("transaction_code = 1 and location_id = #{pump.location_id}").group("extract(month from transaction_time)")
     #Prepare data
     chart_data_array = []
     (Date.today.month-5..Date.today.month).each do |month|
-      pumped_in_month = @dispensed_by_month.select{|obj| obj.month == month }[0].total
+      pumped_in_month = dispensed_by_month.select{|obj| obj.month == month }[0].total
       chart_data_array.push({month: month, total: pumped_in_month})
     end
     #Create json chart obj
