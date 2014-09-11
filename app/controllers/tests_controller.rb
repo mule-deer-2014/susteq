2d8a8bfb0d989347126905f96b7cf25676abeb97
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-10 23:48:16 -0700

    added more methods

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,14 @@
+class TestsController < ApplicationController
+  def test
+    @pump = Pump.first
+    @kiosk = Kiosk.first
+    @provider = Provider.first
+    @viz_data_pump = dispensed_by_month(@pump)
+    @viz_data_kiosk = credits_by_month(@kiosk)
+    @viz_data_credits_by_month = credits_by_kiosk_by_month
+    @viz_data_credits_by_kiosk = credits_by_kiosk_for_all
+    @viz_data_credits_by_kiosk_for_provider = credits_by_kiosk_for_provider(@provider)
+    @viz_data_dispensed_by_pump_for_provider = dispensed_by_pump_for_provider(@provider)
+    render 'test'
+  end
+end
