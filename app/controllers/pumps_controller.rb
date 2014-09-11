f684a7bc3c5730cff04323f5adf21b261d66818d
Igor Gaelzer <igor@nordweg.com>
2014-09-11 15:18:22 -0700

    Resolves merge conflicts in pumps_controller

--- app/controllers/pumps_controller.rb
+++ app/controllers/pumps_controller.rb
@@ -5,17 +5,7 @@ class PumpsController < ApplicationController
 
   def index
     @pumps = current_provider.pumps
-<<<<<<< HEAD
-    sum = 0
-    @pumps.each do |p|
-      sum += p.water_dispensed
-    end
-    @total_dispensed = sum
-    hubs = getHubs
-    @viz_data = [hubs].to_json
-=======
     @viz_data = [dispensed_by_pump_for_provider(current_provider), getHubs].to_json
->>>>>>> origin/new-in-process-master
     @total_dispensed = @pumps.reduce(0) { |sum, pump| sum + pump.water_dispensed }
   end
 
