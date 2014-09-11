8f2a4cb02e62965c8f03377c58e6fecd0a5fea53
Ori <opleban@gmail.com>
2014-09-10 19:40:39 -0700

    assigns output for getHubs call to hubs variable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,9 +3,12 @@ class Admin::DashboardController < ApplicationController
   before_filter :require_admin_signin
 
   def main
-    @new_hubs_ids = (Transaction.all - Hub.get_all_transactions).map { |transaction| transaction.location_id }
+    @new_hubs_ids = (Transaction.all - Hub.get_all_transactions)
+    .map{ |transaction| transaction.location_id }
 
-    @viz_data = [PerspectiveSummary.new.getHubs].to_json
+    hubs = PerspectiveSummary.new.getHubs
+
+    @viz_data = [hubs].to_json
   end
 
   def operations
