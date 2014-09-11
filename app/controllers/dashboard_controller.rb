6ef30456c493ff970c1996665cfb0981a71ca50f
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-11 10:58:49 -0700

    rmoved unneeded @hubs varialbe

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,7 @@ class DashboardController < ApplicationController
   before_filter :require_employee_signin
 
   def main
-    @hubs = getHubs
-    @viz_data = [@hubs].to_json
+    @viz_data = [getHubs].to_json
     render "dashboard/main"
   end
 end
