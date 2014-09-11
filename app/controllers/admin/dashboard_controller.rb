3ed6467720e2a9203df2b4b6399e7e3e24085640
Alex Birdsall <ambirdsall@gmail.com>
2014-09-11 13:50:13 -0700

    Sends mapping data to Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,7 +4,7 @@ class Admin::DashboardController < ApplicationController
   before_filter :require_admin_signin
 
   def main
-    @viz_data = 0
+    @viz_data = [getHubs].to_json
     @new_hubs_ids = (Transaction.all - Hub.get_all_transactions).map { |transaction| transaction.location_id }
   end
 end
