835e520bfd070b81a319c4b9cf0b274bdcb6670f
Eric Kennedy <ekeric13@gmail.com>
2014-09-11 20:54:47 -0700

    got ready to merge

--- app/controllers/admin/operations_controller.rb
+++ app/controllers/admin/operations_controller.rb
@@ -3,11 +3,10 @@ class Admin::OperationsController < ApplicationController
 
 	def index
     @sms = sms_balance_by_pump_table
-    # @last_transaction
     @total_credits_sold = credits_by_kiosk_for_all_table
     @shop_bought = credits_bought_by_kiosk_table
     @total_water = dispensed_by_pump_for_all_table
     @shopkeepers_credit = credits_remaining_by_kiosk_table
-    # @error_codes = # for thirty days  end
+    @error_codes = errors_by_hub
   end
 end
