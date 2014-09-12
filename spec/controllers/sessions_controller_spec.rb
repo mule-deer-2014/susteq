39431af8f26a34a58d0a485c2f2819b63bed989d
Ori <opleban@gmail.com>
2014-09-11 20:24:24 -0700

    solves merge conflict in specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,25 +14,6 @@ describe SessionsController do
     before do
       admin = create(:admin)
       authorize_admin(admin)
-      get :index, format: 'json'
-    end
-
-    describe 'GET sessions#index' do
-      it 'returns a successful status' do
-        assert_response :success
-      end
-
-      it 'responds with valid json' do
-        expect {
-            JSON.parse(response.body)
-          }.to_not raise_error
-      end
-
-      it 'responds with admin permissions' do
-        get :index, format: 'json'
-        expected_response = {permission: 'admin'}.to_json
-        expect(response.body).to eq(expected_response)
-      end
     end
 
     describe 'GET sessions#new' do
@@ -47,14 +28,6 @@ describe SessionsController do
     before do
       @employee = create(:employee, :with_password)
       authorize_employee(@employee)
-    end
-
-    describe 'GET sessions#index' do
-      it 'responds with employee permissions' do
-        get :index, format: 'json'
-        expected_response = {permission: @employee.provider.id}.to_json
-        expect(response.body).to eq(expected_response)
-      end
     end
 
     describe 'GET sessions#new' do
