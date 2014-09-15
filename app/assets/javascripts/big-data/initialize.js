a6c494f028b21763cb1254c8b05c2148d83e35af
Ori <opleban@gmail.com>
2014-09-14 19:48:51 -0700

    creates BigData.initialize function, which is called in each Paloma Controller

--- app/assets/javascripts/big-data/initialize.js
+++ app/assets/javascripts/big-data/initialize.js
@@ -1,3 +1,4 @@
-// $(document).ready(function(){
-//   var dataController = new BigData.DataController();
-// });
+BigData.initialize = function(){
+    var dataController = new BigData.DataController();
+    dataController.getChartData(that.params.viz_data)
+  };
